c762c63a4fa47e0351a3c8d468914630004aee81
Chris Haig <chaig200@caledonian.ac.uk>
2019-04-16 21:51:49 +0100

    Login functionality and login page implemented

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('login.html')
 
   mainWindow.on('closed', function () {
     mainWindow = null
--- renderer.js
+++ renderer.js
@@ -1,3 +1,46 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+
+
+module.exports = {
+ login : function(){
+        var user = document.getElementById("uname").value
+        var password = document.getElementById("psw").value
+        var params = {Username: user,Password: password};
+        var data = new FormData();
+        for(name in params) {
+            data.append(name, params[name]);
+          }
+        
+        var xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
+        
+        xhr.addEventListener("readystatechange", function () {
+          if (this.readyState === 4) {
+              if(this.responseText == "No rows"){
+                  alert("Username or Password missing");
+              }else{
+                var json = JSON.parse(this.responseText);
+                switch(json.Response){
+                    case "Success":
+                    window.location.href = "index.html";
+                    break;
+
+                    case "Invalid Request":
+                    alert("Something went wrong with you're request.\nPlease try again.");
+                    break;
+
+                    case "Invalid login credentials":
+                    alert("Login credentials invalid.");
+                    break;
+                }
+              }
+            
+          }
+        });
+        
+        xhr.open("POST", "http://diversitydevelopment.net/divergentdev/API/login.php");
+        xhr.setRequestHeader("Cache-Control", "no-cache");
+ 
+        xhr.send(data);
+
+ }
+};
